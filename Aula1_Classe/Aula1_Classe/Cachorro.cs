796ef4c449aec1b2abbd77339efbabcb0f61c55a
Karenur <68090656+Karenur@users.noreply.github.com>
2022-03-21 10:55:21 -0300

    Add files via upload

---
+++ Aula1_Classe/Aula1_Classe/Cachorro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_Classe
+{
+    class Cachorro
+    {
+        public String   nome;
+        public String   raca;
+        public int      idade;
+        public float    peso;
+
+        public void Latir()
+        {
+
+            Console.WriteLine($"{nome} está latindo.");
+
+        }
+
+        public void AbanarRabo()
+        {
+
+            Console.WriteLine($"{nome} está abando o rabo");
+
+        }
+
+    }
+}
